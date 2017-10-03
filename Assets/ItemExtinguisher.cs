307d5c88791dc1d830bf247c8c55854f76fb85f1
sivorplanoi <sivorplanoi@gmail.com>
2017-10-03 18:56:26 +0900

    StepAsideUnityChanプロジェクト作成～lesson6発展課題まで
    StepAsideUnityChanプロジェクトを作成し、lesson6課題/発展課題に取り組んだファイル

---
+++ Assets/ItemExtinguisher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemExtinguisher : MonoBehaviour {
+
+	// MainCameraオブジェクトを入れる
+	private GameObject mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		// MainCameraオブジェクト取得
+		this.mainCamera = GameObject.Find ("Main Camera");
+
+		// 衝突判定用壁オブジェクトをMeinCameraのすぐ後ろに配置
+		this.transform.position = new Vector3 (0, this.transform.position.y, mainCamera.transform.position.z - 1);
+				
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 衝突安定用壁オブジェクトがMainCameraの動きに追従
+		this.transform.position = new Vector3 (0, this.transform.position.y, mainCamera.transform.position.z - 1);
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		// 接触したゲームオブジェクトを破棄(MainCameraから見えなくなったアイテムを順次破棄)
+		Destroy(other.gameObject);
+
+	}
+
+
+}
